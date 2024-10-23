c42c28bdd6183a645cbc3483946b993f1254ef17
sabihajyoti22 <sabihajyoti@gmail.com>
2024-10-23 21:10:39 +0600

    chore: add vercel file

--- dist/Config/config.js
+++ dist/Config/config.js
@@ -1,6 +1,5 @@
 "use strict";
-// const path = require('path')
-// require("dotenv").config({ path: path.resolve(__dirname, '../../.env') })
+Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv").config();
 const dev = {
     app: {
--- dist/Routes/ticket.route.js
+++ dist/Routes/ticket.route.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const { createTicket } = require("../Controller/ticket.controller");
 const router = express.Router();
